fd66240c4b6346b9d4f843ba772a88b9adc89f19
Shiva Choudhary <shivachaudhary537@gmail.com>
2024-07-29 20:23:14 +0530

    Investment Details Created

--- src/main/java/com/scbsena/demo/controller/FinancialAdvisoryController.java
+++ src/main/java/com/scbsena/demo/controller/FinancialAdvisoryController.java
@@ -1,13 +1,12 @@
 package com.scbsena.demo.controller;
+import Model.InvestmentDetailsDto;
 import Model.UserDetailsDto;
-import com.scbsena.demo.service.CustomerData;
-import com.scbsena.demo.service.FinancialAdvisoryService;
-import com.scbsena.demo.service.LoginService;
-import com.scbsena.demo.service.OpenAIService;
+import com.scbsena.demo.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +29,9 @@ public class FinancialAdvisoryController {
 
     @Autowired
     private CustomerData customerData;
+
+    @Autowired
+    private InvestmentDetailsService investmentDetailsService;
 
     @GetMapping("/getAdvice")
     public String getFinancialAdvice(@RequestBody String query) throws Exception {
@@ -54,6 +56,26 @@ public class FinancialAdvisoryController {
             return new ResponseEntity<String>("Data Saved Successfully", HttpStatus.OK);
         } catch (Exception e){
             return new ResponseEntity<String>("Error in saving data", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/investmentDetails")
+    public ResponseEntity<?> saveInvestmentDetails(@RequestBody InvestmentDetailsDto request){
+        try {
+            investmentDetailsService.save(request);
+            return new ResponseEntity<String>("Success", HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_GATEWAY);
+        }
+    }
+
+    @GetMapping("/getInvestmentDetails")
+    public ResponseEntity<?> getInvestmentDetails(@RequestParam String email){
+        try{
+            InvestmentDetailsDto response = investmentDetailsService.getDetails(email);
+            return new ResponseEntity<InvestmentDetailsDto>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_GATEWAY);
         }
     }
 
